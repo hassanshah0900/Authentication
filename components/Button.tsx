bd0f35004cfe4202489a9a4d78785039e684c9e1
Syed Muhammad Hassan Shah <hasni9267@gmail.com>
2025-04-13 18:58:45 +0500

    refactor: enable adding attributes as props and style Button component in focus state

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,8 +1,16 @@
-import { ReactNode } from "react";
+import { ButtonHTMLAttributes } from "react";
+import { twMerge } from "tailwind-merge";
 
-export default function Button({ children }: { children: ReactNode }) {
+type Props = ButtonHTMLAttributes<HTMLButtonElement>;
+export default function Button({ children, className, ...props }: Props) {
   return (
-    <button className="bg-gradient-to-r text-sm from-cyan-500 to-pink-500 text-white uppercase py-3 px-8 cursor-pointer font-semibold rounded-full w-full">
+    <button
+      {...props}
+      className={twMerge(
+        "bg-gradient-to-r text-sm from-cyan-500 to-pink-500 text-white uppercase py-3 px-8 cursor-pointer font-semibold rounded-full w-full focus-visible:outline-cyan-500 outline-1 outline-offset-2",
+        className
+      )}
+    >
       {children}
     </button>
   );
