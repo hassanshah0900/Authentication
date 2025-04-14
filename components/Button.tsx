ff3e3a1dd527bf2e61624886d81ff14aae1ae7a4
Syed Muhammad Hassan Shah <hasni9267@gmail.com>
2025-04-14 14:27:13 +0500

    refactor: extend Button component to function as link when href attribute is passed.

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,13 +1,30 @@
-import { ButtonHTMLAttributes } from "react";
+import Link from "next/link";
+import { AnchorHTMLAttributes, ButtonHTMLAttributes } from "react";
 import { twMerge } from "tailwind-merge";
 
-type Props = ButtonHTMLAttributes<HTMLButtonElement>;
-export default function Button({ children, className, ...props }: Props) {
+type Props = ButtonHTMLAttributes<HTMLButtonElement> &
+  AnchorHTMLAttributes<HTMLAnchorElement>;
+export default function Button({ children, className, href, ...props }: Props) {
+  if (href) {
+    return (
+      <Link
+        href={href}
+        {...props}
+        className={twMerge(
+          "inline-block bg-gradient-to-r text-sm from-cyan-500 to-pink-500 text-white uppercase py-3 px-8 cursor-pointer font-semibold rounded-full w-full focus-visible:outline-cyan-500 outline-1 outline-offset-2",
+          className
+        )}
+      >
+        {children}
+      </Link>
+    );
+  }
+
   return (
     <button
       {...props}
       className={twMerge(
-        "bg-gradient-to-r text-sm from-cyan-500 to-pink-500 text-white uppercase py-3 px-8 cursor-pointer font-semibold rounded-full w-full focus-visible:outline-cyan-500 outline-1 outline-offset-2",
+        "inline-block bg-gradient-to-r text-sm from-cyan-500 to-pink-500 text-white uppercase py-3 px-8 cursor-pointer font-semibold rounded-full w-full focus-visible:outline-cyan-500 outline-1 outline-offset-2",
         className
       )}
     >
