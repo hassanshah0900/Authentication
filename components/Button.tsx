aae57e897fe1f10329f2485bfbbfdf6efa5eb622
Syed Muhammad Hassan Shah <hasni9267@gmail.com>
2025-04-15 19:27:32 +0500

    refactor: add loading state to Button component

--- components/Button.tsx
+++ components/Button.tsx
@@ -3,8 +3,16 @@ import { AnchorHTMLAttributes, ButtonHTMLAttributes } from "react";
 import { twMerge } from "tailwind-merge";
 
 type Props = ButtonHTMLAttributes<HTMLButtonElement> &
-  AnchorHTMLAttributes<HTMLAnchorElement>;
-export default function Button({ children, className, href, ...props }: Props) {
+  AnchorHTMLAttributes<HTMLAnchorElement> & {
+    isPending?: boolean;
+  };
+export default function Button({
+  children,
+  className,
+  href,
+  isPending = false,
+  ...props
+}: Props) {
   if (href) {
     return (
       <Link
@@ -24,11 +32,15 @@ export default function Button({ children, className, href, ...props }: Props) {
     <button
       {...props}
       className={twMerge(
-        "inline-block bg-gradient-to-r text-sm from-cyan-500 to-pink-500 text-white uppercase py-3 px-8 cursor-pointer font-semibold rounded-full w-full focus-visible:outline-cyan-500 outline-1 outline-offset-2",
+        "flex justify-center items-center bg-gradient-to-r text-sm from-cyan-500 to-pink-500 text-white uppercase py-3 px-8 cursor-pointer font-semibold rounded-full w-full focus-visible:outline-cyan-500 outline-1 outline-offset-2",
         className
       )}
     >
-      {children}
+      {isPending ? (
+        <div className="w-6 h-6 border-4 border-white/20 border-r-white rounded-full animate-spin"></div>
+      ) : (
+        children
+      )}
     </button>
   );
 }
