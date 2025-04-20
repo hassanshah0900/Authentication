ede3010adfbcddbbd59eabf495a95d8a4cc0fdfc
Syed Muhammad Hassan Shah <hasni9267@gmail.com>
2025-04-20 18:55:30 +0500

    create an error map for common supabase auth errors

---
+++ utils/supabase/errorMap.ts
@@ -0,0 +1,6 @@
+export const supabaseErrorMap: { [key: string]: string } = {
+  otp_expired: "Confirmation link has expired",
+  email_not_confirmed: "Your email is not confirmed",
+  invalid_credentials: "Invalid email or password",
+  access_denied: "Access Denied",
+};
