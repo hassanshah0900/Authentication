8400de960615d9cbad76f73db599ad6b6a4366f8
Syed Muhammad Hassan Shah <hasni9267@gmail.com>
2025-04-20 19:34:26 +0500

    refactor: use supabaseErrorMap to show error messages

--- app/auth/signup/page.tsx
+++ app/auth/signup/page.tsx
@@ -3,6 +3,7 @@
 import Button from "@/components/Button";
 import Input from "@/components/Input";
 import { AuthState, signup } from "@/lib/actions/auth-actions";
+import { supabaseErrorMap } from "@/utils/supabase/errorMap";
 import Link from "next/link";
 import { useActionState, useEffect } from "react";
 import { toast, ToastContainer } from "react-toastify";
@@ -16,74 +17,73 @@ export default function SignupPage() {
 
   useEffect(() => {
     if (error) {
-      toast(error, { type: "error" });
+      toast(supabaseErrorMap[error] ?? error, { type: "error" });
     }
   }, [state]);
 
   return (
     <>
       <ToastContainer />
-      <form
-        action={formAction}
-        className="w-full bg-white px-8 py-6 rounded-lg shadow-2xl mt-20 text-center max-w-[320px]"
-      >
-        <h1 className=" text-2xl font-bold text-heading mb-6">Signup</h1>
-        <div className="flex flex-col gap-4">
-          <div>
-            <Input
-              type="text"
-              name="name"
-              placeholder="Name"
-              defaultValue={data?.get("name")?.toString()}
-            />
-            {errors?.name &&
-              errors.name.map((errorMessage) => (
-                <p
-                  key={errorMessage}
-                  className="text-sm text-red-500 text-start"
-                >
-                  {errorMessage}
-                </p>
-              ))}
+      <div className="w-full bg-white px-8 py-6 rounded-lg shadow-2xl mt-20 text-center max-w-[320px]">
+        <form action={formAction}>
+          <h1 className=" text-2xl font-bold text-heading mb-6">Signup</h1>
+          <div className="flex flex-col gap-4">
+            <div>
+              <Input
+                type="text"
+                name="name"
+                placeholder="Name"
+                defaultValue={data?.get("name")?.toString()}
+              />
+              {errors?.name &&
+                errors.name.map((errorMessage) => (
+                  <p
+                    key={errorMessage}
+                    className="text-sm text-red-500 text-start"
+                  >
+                    {errorMessage}
+                  </p>
+                ))}
+            </div>
+            <div>
+              <Input
+                type="text"
+                name="email"
+                placeholder="Email"
+                defaultValue={data?.get("email")?.toString()}
+              />
+              {errors?.email &&
+                errors.email.map((errorMessage) => (
+                  <p
+                    key={errorMessage}
+                    className="text-sm text-red-500 text-start"
+                  >
+                    {errorMessage}
+                  </p>
+                ))}
+            </div>
+            <div>
+              <Input
+                type="password"
+                name="password"
+                placeholder="Password"
+                defaultValue={data?.get("password")?.toString()}
+              />
+              {errors?.password &&
+                errors.password.map((errorMessage) => (
+                  <p
+                    key={errorMessage}
+                    className="text-sm text-red-500 text-start"
+                  >
+                    {errorMessage}
+                  </p>
+                ))}
+            </div>
           </div>
-          <div>
-            <Input
-              type="text"
-              name="email"
-              placeholder="Email"
-              defaultValue={data?.get("email")?.toString()}
-            />
-            {errors?.email &&
-              errors.email.map((errorMessage) => (
-                <p
-                  key={errorMessage}
-                  className="text-sm text-red-500 text-start"
-                >
-                  {errorMessage}
-                </p>
-              ))}
-          </div>
-          <div>
-            <Input
-              type="password"
-              name="password"
-              placeholder="Password"
-              defaultValue={data?.get("password")?.toString()}
-            />
-            {errors?.password &&
-              errors.password.map((errorMessage) => (
-                <p
-                  key={errorMessage}
-                  className="text-sm text-red-500 text-start"
-                >
-                  {errorMessage}
-                </p>
-              ))}
-          </div>
-        </div>
-        <Button isPending={isPending} className="mt-8 mb-2">
-          Sign Up
-        </Button>
+          <Button isPending={isPending} className="mt-8 mb-2">
+            Sign Up
+          </Button>
+        </form>
         <span className="text-sm text-body ">
           Already have an account?{" "}
           <Link
@@ -93,7 +93,7 @@ export default function SignupPage() {
             Signin
           </Link>
         </span>
-      </form>
+      </div>
     </>
   );
 }
