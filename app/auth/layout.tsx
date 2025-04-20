80315f17d99109ce4630659620315c4e71e68442
Syed Muhammad Hassan Shah <hasni9267@gmail.com>
2025-04-20 19:14:32 +0500

    fix: set min height to 100vh so that its children don't overflow

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 
 function AuthLayout({ children }: { children: ReactNode }) {
   return (
-    <main className="flex justify-center items-start bg-gradient-to-r from-cyan-500 to-pink-500 h-screen">
+    <main className="flex justify-center items-start bg-gradient-to-r from-cyan-500 to-pink-500 min-h-screen pb-10">
       {children}
     </main>
   );
