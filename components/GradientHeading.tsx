698877451ce92d0de17098812bdc058b38e373f9
Syed Muhammad Hassan Shah <hasni9267@gmail.com>
2025-04-20 19:16:45 +0500

    fix: make the line height 1.3 fo font size

--- components/GradientHeading.tsx
+++ components/GradientHeading.tsx
@@ -2,7 +2,10 @@ import { ReactNode } from "react";
 
 export default function GradientHeading({ children }: { children: ReactNode }) {
   return (
-    <h2 className="text-5xl text-center font-bold w-max leading-loose bg-gradient-to-r from-cyan-500 to-pink-500 bg-clip-text text-transparent">
+    <h2
+      style={{ lineHeight: 1.3 }}
+      className="text-5xl text-center font-bold bg-gradient-to-r from-cyan-500 to-pink-500 bg-clip-text text-transparent"
+    >
       {children}
     </h2>
   );
