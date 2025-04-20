03fb547ab3618b064c91769bcc3408ca39e84ed0
Syed Muhammad Hassan Shah <hasni9267@gmail.com>
2025-04-20 19:30:04 +0500

    refactor: send the user to an error page if the email link has expired.

--- app/auth/confirm/route.ts
+++ app/auth/confirm/route.ts
@@ -21,7 +21,9 @@ export async function GET(request: NextRequest) {
   });
 
   if (error) {
-    throw error;
+    redirect(
+      `/auth/auth-code-error?error=${error.code}&error_description=${error.message}`
+    );
   }
 
   redirect(next);
