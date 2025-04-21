5f8cf20a122f9a4f460d4867e71ede7bc7e2ee20
Syed Muhammad Hassan Shah <hasni9267@gmail.com>
2025-04-21 17:14:47 +0500

    create update password page

---
+++ app/auth/update-password/page.tsx
@@ -0,0 +1,73 @@
+"use client";
+
+import Button from "@/components/Button";
+import Card from "@/components/Card";
+import Input from "@/components/Input";
+import { AuthState, updatePassword } from "@/lib/actions/auth-actions";
+import { useActionState, useEffect } from "react";
+import { toast, ToastContainer } from "react-toastify";
+
+export default function UpdatePasswordPage() {
+  const [state, formAction, isPending] = useActionState<AuthState, FormData>(
+    updatePassword,
+    {}
+  );
+  const { validationErrors, data, error, message, success } = state;
+
+  useEffect(() => {
+    if (error) toast(error, { type: "error" });
+
+    if (success) {
+      toast(message, { type: "success", autoClose: false });
+    }
+  }, [state]);
+
+  return (
+    <Card className="mt-20">
+      <ToastContainer />
+      <h1 className="text-2xl font-bold text-heading mb-6">Update Password</h1>
+
+      <form action={formAction} className="w-[320px]">
+        <div className="space-y-4">
+          <div>
+            <Input
+              name="password"
+              type="password"
+              defaultValue={data?.get("password")?.toString()}
+              placeholder="Password"
+            />
+            {validationErrors?.password &&
+              validationErrors.password.map((errorMessage) => (
+                <p
+                  key={errorMessage}
+                  className="text-sm text-red-500 text-start"
+                >
+                  {errorMessage}
+                </p>
+              ))}
+          </div>
+          <div>
+            <Input
+              type="password"
+              name="confirmPassword"
+              defaultValue={data?.get("confirmPassword")?.toString()}
+              placeholder="Confirm Password"
+            />
+            {validationErrors?.confirmPassword &&
+              validationErrors.confirmPassword.map((errorMessage) => (
+                <p
+                  key={errorMessage}
+                  className="text-sm text-red-500 text-start"
+                >
+                  {errorMessage}
+                </p>
+              ))}
+          </div>
+        </div>
+        <Button isPending={isPending} className="mt-10">
+          Submit
+        </Button>
+      </form>
+    </Card>
+  );
+}
