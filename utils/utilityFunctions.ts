8a912d44b9be4518fc9fcf86a9896b80587e604f
Syed Muhammad Hassan Shah <hasni9267@gmail.com>
2025-04-23 15:00:00 +0500

    create isAuthenticated utility function for check if user is authenticated or not

--- utils/utilityFunctions.ts
+++ utils/utilityFunctions.ts
@@ -1,3 +1,13 @@
+import { createClient } from "./supabase/server";
+
 export function getFormFieldValue(value: FormDataEntryValue | null) {
   return value || undefined;
+}
+
+export async function isAuthenticated() {
+  const supabase = await createClient();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+  return user;
 }
