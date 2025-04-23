1b481b425773fecc62bf498fbf075bb6447af628
Syed Muhammad Hassan Shah <hasni9267@gmail.com>
2025-04-23 14:59:03 +0500

    setup a loading indicator for all auth routes

---
+++ app/auth/loading.tsx
@@ -0,0 +1,7 @@
+export default function loading() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div className="h-20 w-20 rounded-full border-4 border-white/20 border-r-white animate-spin"></div>
+    </div>
+  );
+}
